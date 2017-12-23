7c0e194e2ba522ce9dd3cc956c8356768e193949
Your Name <you@example.com>
2017-12-23 10:41:25 -0800

    dnssec test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 echo -en "$CONFIG" > /etc/unbound/unbound.conf.d/50-config.conf
 
 ## start
+unbound-anchor || exit
+
 rm -f /var/run/unbound.pid
 exec /usr/sbin/unbound -d -c /etc/unbound/unbound.conf "$@"
