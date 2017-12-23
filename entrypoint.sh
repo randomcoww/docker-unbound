c618801166da4df07b6a20775628dfbc28f64e8c
Your Name <you@example.com>
2017-12-23 11:46:02 -0800

    dnssec perm issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ echo -en "$CONFIG" > /etc/unbound/unbound.conf.d/50-config.conf
 
 ## start
 unbound-anchor || exit
+chown -R unbound /usr/share/dnssec-root
 
 rm -f /var/run/unbound.pid
 exec /usr/sbin/unbound -d -c /etc/unbound/unbound.conf "$@"
